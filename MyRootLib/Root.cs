bab2a6e9bf700de991287a1ff5f87af7e08ce053
Swifty1235 <104468694+Swifty1235@users.noreply.github.com>
2024-02-14 22:36:04 -0500

    Unit 3 Lab

---
+++ MyRootLib/Root.cs
@@ -0,0 +1,19 @@
+﻿namespace MyRootLib
+{
+    public class Root
+    {
+        /// <summary>
+        /// this method calculates the square of the number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static double MyRoot(double number)
+        {
+            double root;
+            // Correctly using Math.Pow to calculate the square of the number
+            root = Math.Pow(number, 2); // This will calculate number^2, not the square root
+            Console.WriteLine("The square of " + number + " is " + root);
+            return root;
+        }
+    }
+}
